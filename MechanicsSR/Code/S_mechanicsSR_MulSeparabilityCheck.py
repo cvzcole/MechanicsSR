a01aad896a55045d54b4b7db9c39bd1cf2315cc0
cvzcole <cvzcole@gatech.edu>
2025-05-09 16:25:34 -0400

    Update S_mechanicsSR_MulSeparabilityCheck.py

--- MechanicsSR/Code/S_mechanicsSR_MulSeparabilityCheck.py
+++ MechanicsSR/Code/S_mechanicsSR_MulSeparabilityCheck.py
@@ -15,10 +15,19 @@ from itertools import combinations
 import time
 import math
 
+def turn2list(value):
+    if isinstance(value, tuple):
+        return list(value)
+    elif isinstance(value, int):
+        return [value]
+    elif isinstance(value, list):
+        return value
+
 def check_separability_mul(pathdir, filename):
     # separability tolerance (hyperparameter)
     tolerance = 0.05
-    print("\n", "Start to check multiplication separability \n")
+    print(" ")
+    print("Start to check multiplication separability \n")
     # compare first 4 digits
     digits_tolerance = 1e-5
     # load the data
@@ -61,12 +70,12 @@ def check_separability_mul(pathdir, filename):
     for i in range(1,n_variables):
         c = combinations(var_indices_list, i)
         for j in c:
-            print('Now check ith variable_mul , idx = ',j)
             x_bary_idx=[]
             x_bary_result=[]
             fact_vary_one = factors.clone()
             fact_vary_rest = factors.clone()
             rest_indx = list(filter(lambda x: x not in j, var_indices_list))
+            print('Now check i =', turn2list(j), 'j =', turn2list(rest_indx))
             for t1 in rest_indx:
                 xy_bar_result=[]
                 fact_vary_one[:,t1] = torch.full((len(factors),),torch.median(factors[:,t1]))
@@ -132,16 +141,17 @@ def check_separability_mul(pathdir, filename):
             
             # if error is above tolerance then quit
             if mse>tolerance:
-                print('no multiplication separability Er_total=',mse)
+                print('no multiplication separability Er_total =',mse)
                 return 99999,j,rest_indx
 
             if mse<=tolerance:
-                print('Multiplication separability found, er=',mse)
-                print('variable_idx = ', j)
+                print('Multiplication separability found, er =',mse)
+                #print('variable_idx = ', j)
                 if mse < min_error:
                     min_error = mse
-                    best_i = j
-                    best_j = rest_indx
-                print("\n", "Best multiplicativity separation so far: i =", best_i ,"j =", best_j, "error =", min_error, "\n")
+                    best_i = turn2list(j)
+                    best_j = turn2list(rest_indx)
+                print(" ")
+                print("Best additivity separation so far: i =", best_i ,"j =", best_j, "error =", min_error, "\n")
                     
     return min_error, best_i, best_j
