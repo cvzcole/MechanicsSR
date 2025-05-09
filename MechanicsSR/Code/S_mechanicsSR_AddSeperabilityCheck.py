c32fe61dc033fde1456b078ec0617874ac322417
cvzcole <cvzcole@gatech.edu>
2025-05-09 16:25:54 -0400

    Update S_mechanicsSR_AddSeperabilityCheck.py

--- MechanicsSR/Code/S_mechanicsSR_AddSeperabilityCheck.py
+++ MechanicsSR/Code/S_mechanicsSR_AddSeperabilityCheck.py
@@ -157,12 +157,12 @@ def check_separability_plus(pathdir, filename):
             
             # if error is above tolerance then quit
             if mse>tolerance:
-                print('no additive separability Er_total=',mse)
+                print('no additive separability Er_total =',mse)
                 return 99999,j,rest_indx
 
             if mse<=tolerance:
-                print('addittive separability found, er=',mse)
-                print('variable_idx = ', j)
+                print('addittive separability found, er =',mse)
+                #print('variable_idx = ', j)
                 if mse < min_error:
                     min_error = mse
                     best_i = turn2list(j)
